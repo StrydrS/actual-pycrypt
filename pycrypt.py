b9f4f7b30fb78ae0cc6d3b4606df04156bf35b21
strydr <strydr8@gmail.com>
2018-05-17 23:58:25 -0700

    works!

--- pycrypt.py
+++ pycrypt.py
@@ -1,6 +1,15 @@
-<<<<<<< HEAD
 import string
 import random
+
+def generateNumber():
+    newString = ""
+    
+    for x in range(0,5):
+        newString = newString + str(random.randint(0,9))
+        
+    newInt = int(newString)
+    return newInt
+    
 def generateList():
     
     allChars = string.printable
@@ -8,35 +17,10 @@ def generateList():
     lenArray = len(allChars)
     totalArray = [0]*lenArray
     for i in range (0, lenArray):
-        totalArray[i] = 54235
+        totalArray[i] = generateNumber()
     print(totalArray)
-    print(lenArray)
-    print(allChars)
     
 def main():
     generateList()
 
 main()
-=======
-import random
-
-def generateList():
-    
-    
-def generateNumber():
-    newString = ""
-    for x in range(0,5):
-        newString = newString + str(random.randint(0,9))
-    newInt = int(newString)
-
-def main():
-    userSeed = int(input("What is your seed?"))
-    random.seed(userSeed)
-    
-    generateList()
-    
-    
-    
-    
-main()
->>>>>>> 0b3a32a91495649972c2fb4b5a80c06899ea4219
